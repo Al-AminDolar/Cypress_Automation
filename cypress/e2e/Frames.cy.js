9aecc034fdd9e9af4112acb6f8641d7a658e99c9
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-05 21:41:43 +0600

    handeling iframe

---
+++ cypress/e2e/Frames.cy.js
@@ -0,0 +1,5 @@
+desribe("handaling frames", () => {
+  it("appproach 1", () => {
+    cy.visit("https://the-internet.herokuapp.com/iframe");
+  });
+});
--- cypress/e2e/Tabs.cy.js
+++ cypress/e2e/Tabs.cy.js
@@ -31,37 +31,3 @@ describe("Handle child Tabs", () => {
     cy.go("back"); // back to parrent tab
   });
 });
-
-describe("Handle child Tabs", () => {
-  it("Approch1", () => {
-    cy.visit("https://the-internet.herokuapp.com/windows"); //parent Tab
-    cy.get(".example > a").invoke("removeAttr", "target").click(); //clicking on link
-    cy.url().should(
-      "include",
-      "https://the-internet.herokuapp.com/windows/new"
-    );
-
-    cy.wait(5000); //wait
-    //operations
-    cy.go("back"); // back to parrent tab
-  });
-
-  it("Approch2", () => {
-    cy.visit("https://the-internet.herokuapp.com/windows"); //parent Tab
-
-    cy.get(".example > a").then((e) => {
-      let url = e.prop("href");
-
-      cy.visit(url);
-    });
-
-    cy.url().should(
-      "include",
-      "https://the-internet.herokuapp.com/windows/new"
-    );
-
-    cy.wait(5000); //wait
-    //operations
-    cy.go("back"); // back to parrent tab
-  });
-});
