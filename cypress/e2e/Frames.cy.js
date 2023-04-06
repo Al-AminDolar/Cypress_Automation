2c92abb3925738372237744d8eef6566ef1d9973
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-06 20:29:34 +0600

    iframes approach1 updated

--- cypress/e2e/Frames.cy.js
+++ cypress/e2e/Frames.cy.js
@@ -1,5 +1,16 @@
-desribe("handaling frames", () => {
+// source link:https://youtu.be/CTpSOW-oL7k Is a Great Engineer
+
+describe("handaling frames", () => {
   it("appproach 1", () => {
     cy.visit("https://the-internet.herokuapp.com/iframe");
+    const iframe = cy
+      .get("#mce_0_ifr")
+      .its("0.contentDocument.body")
+      .should("be.visible")
+      .then(cy.wrap);
+
+    iframe.clear().type("Dollar Is a Great Engineer {ctrl+a}");
+
+    cy.get("[aria-label='Bold']").click();
   });
 });
