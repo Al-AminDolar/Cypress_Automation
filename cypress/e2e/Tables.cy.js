742d05d5092c7dc4149f87659a8f6b46afb03aab
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-08 23:49:34 +0600

    Handle Tables in processing

--- cypress/e2e/Tables.cy.js
+++ cypress/e2e/Tables.cy.js
@@ -1,6 +1,14 @@
 describe("Handles Tables", () => {
   beforeEach("Login", () => {
+    cy.visit("https://demo.opencart.com/admin/index.php?route=common/login");
     cy.get("#input-username").type("demo");
-    cy.get("input-password").type("demo");
+    cy.get("#input-password").type("demo");
+    cy.get("button[type='submit']").click();
+    cy.get("button[class='btn-close']").click();
+    cy.get("#menu-customer>a").click();
+    cy.get("#menu-customer>ul>li:first-child").click();
+  });
+  it("URL Testing", () => {
+    cy.get("h1").should("have.text", "Customers");
   });
 });
