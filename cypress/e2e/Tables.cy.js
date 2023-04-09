c2869f7b00df1a97c70047d88e86befb20b73fe4
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-09 10:27:30 +0600

    red all data from row and column

--- cypress/e2e/Tables.cy.js
+++ cypress/e2e/Tables.cy.js
@@ -9,7 +9,7 @@ describe("Handles Tables", () => {
     cy.get("#menu-customer>ul>li:first-child").click();
   });
 
-  it("Count Rows approch-1", () => {
+  it.skip("Count Rows approch-1", () => {
     cy.get(".table.table-bordered.table-hover>thead>tr")
       .children()
       .its("length")
@@ -18,7 +18,7 @@ describe("Handles Tables", () => {
       });
   });
 
-  it("Count Rows approach-2", () => {
+  it.skip("Count Rows approach-2", () => {
     cy.get(".table.table-bordered.table-hover>thead>tr")
       .children()
       .its("length")
@@ -29,5 +29,43 @@ describe("Handles Tables", () => {
           cy.log("There is no table on this page");
         }
       });
+  });
+
+  it.skip("Count Columns approach-1", () => {
+    cy.get(".table.table-bordered.table-hover>tbody>tr")
+      .its("length")
+      .then((count) => {
+        cy.log(`There are ${count} Columns in the table`);
+      });
+  });
+
+  it.skip("count columns approach-2", () => {
+    cy.get(".table.table-bordered.table-hover>tbody>tr")
+      .its("length")
+      .then((count) => {
+        if (count > 0) {
+          cy.log(`There are ${count} Columns in the table`);
+        } else {
+          cy.log(`There are no Columns in the table`);
+        }
+      });
+  });
+
+  it.skip("Check  cell data from specific row and column", () => {
+    cy.get(
+      ".table.table-bordered.table-hover>tbody>tr:nth-child(5)>td:nth-child(3)"
+    ).contains("princytrainings4@gmail.com");
+  });
+
+  it.only("Red all the rows and columns data from 1st page", () => {
+    cy.get("table[class='table table-bordered table-hover']>tbody>tr").each(
+      ($row, index, $rows) => {
+        cy.wrap($row).within(() => {
+          cy.get("td").each(($col, index, $cols) => {
+            cy.log($col.text());
+          });
+        });
+      }
+    );
   });
 });
