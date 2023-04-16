90a10a96596de6aac35af48fcae2303b790323c9
Al-AminDolar <alamin.dolar1@gmail.com>
2023-04-16 08:58:57 +0600

    navigation test run

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -2,11 +2,27 @@ require("cypress-xpath");
 // go
 // back
 // reload
+
 describe("mysuite", () => {
   it("NavigationTest", () => {
     cy.visit("https://demo.opencart.com/");
     cy.title().should("eq", "Your Store");
-    cy.get("li:nth-child(7) a:nth-child(1)");
+
+    cy.get("li:nth-child(7) a:nth-child(1)").click();
     cy.get("div[id='content'] h2").should("have.text", "Cameras");
+
+    cy.go("back"); //home
+    cy.title().should("eq", "Your Store");
+
+    cy.go("forward"); //cameras
+    cy.get("div[id='content'] h2").should("have.text", "Cameras");
+
+    cy.go(-1); //home
+    cy.title().should("eq", "Your Store");
+
+    cy.go(1); //cameras
+    cy.get("div[id='content'] h2").should("have.text", "Cameras");
+
+    cy.reload();
   });
 });
